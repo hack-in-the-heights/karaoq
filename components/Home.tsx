420acf728acc348228788e2527275f52ab023e65
Anna Perdomo <annamaliaperdomo@gmail.com>
2022-09-24 12:32:53 -0700

    feat: Create dynamic join code input for home

--- components/Home.tsx
+++ components/Home.tsx
@@ -3,28 +3,48 @@ import * as React from 'react';
 import styles from '../styles/Home.module.css';
 
 const Home = (): React.ReactElement => {
+  const [displayJoinCodeInput, setDisplayJoinCodeInput] =
+    React.useState<boolean>(false);
+  console.log({ displayJoinCodeInput });
   return (
-      <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to KaraoQ</h1>
+    <main className={styles.main}>
+      <h1 className={styles.title}>Welcome to KaraoQ</h1>
 
-        <p className={styles.description}>
-          Your one stop shop for Youtube Karaoke!
-        </p>
+      <p className={styles.description}>
+        Your one stop shop for Youtube Karaoke!
+      </p>
 
-        <div className={styles.grid}>
-          {/* FIXME: Make links dynamic */}
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Host &rarr;</h2>
-            <p>Create a karaoke queue that other people can join.</p>
-          </a>
+      <div className={styles.grid}>
+        {/* FIXME: Make links dynamic */}
+        <button className={styles.card}>
+          <h2>Host &rarr;</h2>
+          <p>Create a karaoke queue that other people can join.</p>
+        </button>
 
-          <button className={styles.card}>
-            <h2>Play &rarr;</h2>
-            <p>Join a pre-existing queue that someone else set up.</p>
-          </button>
-        </div>
-      </main>
-    );
+        <button
+          className={styles.card}
+          onClick={(e): void => {
+            setDisplayJoinCodeInput(!displayJoinCodeInput);
+          }}
+        >
+          <h2>Play &rarr;</h2>
+          <p>Join a pre-existing queue that someone else set up.</p>
+
+          {displayJoinCodeInput && (
+            <div className={styles.inputContainer}>
+              <label>Enter join code: </label>
+              <input
+                id="enterJoinCode"
+                name="enterJoinCode"
+                required
+                type="text"
+              />
+            </div>
+          )}
+        </button>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
