174117671f0cf6cc24b54c5d4fd567fa1cf9a098
Corrie Grimshaw <corrie.e.grimshaw@gmail.com>
2022-09-24 16:25:44 -0700

    Add join code to Host page

--- components/Host.tsx
+++ components/Host.tsx
@@ -2,13 +2,17 @@ import * as React from 'react';
 
 import styles from '../styles/Host.module.css';
 
+import {useRouter} from 'next/router';
+
 const video_id = "-PnwATAyJJU"
 const room_code = "TVXQ"
 
 const Host = (): React.ReactElement => {
+  const router = useRouter();
+  const joinCode = router.query.joinCode;
   return (
     <main className={styles.main}>
-       <h1 className={styles.title}>TO JOIN: Go to <a href={"http://www.karoq.vercel.app"}>www.karoq.vercel.app</a> and type in code <span className={styles.span}>{room_code}</span></h1>
+       <h1 className={styles.title}>TO JOIN: Go to <a href={"http://karoq.vercel.app"}>karoq.vercel.app</a> and type in code <span className={styles.span}>{joinCode}</span></h1>
        <iframe className={styles.video}
         src={"https://www.youtube.com/embed/" + video_id}
         ></iframe>
