483da27ddb2fcbe86bd675c07778da8a22ea6b2f
Corrie Grimshaw <corrie.e.grimshaw@gmail.com>
2022-09-25 16:24:59 -0700

    Add logic for empty queue to /Host

--- components/Host.tsx
+++ components/Host.tsx
@@ -2,19 +2,32 @@ import * as React from 'react';
 
 import styles from '../styles/Host.module.css';
 
+import getInitialQueue from "../app/queue/getInitialQueue.ts"
+
 import {useRouter} from 'next/router';
 
-const video_id = "bNOncnlm7Ho"
-
 const Host = (): React.ReactElement => {
+  const [queue, setQueue] = React.useState([])
   const router = useRouter();
-  const joinCode = router.query.joinCode;
+  const joinCode = "asdf"//router.query.joinCode;
+
+  React.useEffect(()=>{
+    getInitialQueue(joinCode).then((res)=> setQueue(res));
+  })
+
+  //TO-DO
+  function playNextInQueue(){
+
+  }
+
+
   return (
     <main className={styles.main}>
        <h1 className={styles.title}>TO JOIN: Go to <a href={"http://karaoq.vercel.app"}>karaoq.vercel.app</a> and type in code <span className={styles.span}>{joinCode}</span></h1>
-       <iframe className={styles.video}
-        src={"https://www.youtube.com/embed/" + video_id}
-        ></iframe>
+       {queue.length > 0 ?
+        <iframe className={styles.video}
+          src={"https://www.youtube.com/embed/" + queue[0].video_id}
+        ></iframe> : <h3>{"No songs in queue. :("}</h3>}
     </main>
   );
 };
