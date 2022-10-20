73d0bbad9d9fc7cf860fd9b8e878a94dda5535fc
Corrie Grimshaw <corrie@pcg.local>
2022-10-20 14:13:50 -0700

    Refactor: fix typescript errors

--- components/Host.tsx
+++ components/Host.tsx
@@ -2,17 +2,18 @@ import * as React from 'react';
 
 import styles from '../styles/Host.module.css';
 
-import getInitialQueue from "../app/queue/getInitialQueue.ts"
+import getInitialQueue from "../app/queue/getInitialQueue"
 
-import {useRouter} from 'next/router';
+import { useRouter } from 'next/router';
+import { QueueEntry } from '../pages/api/types';
 
 const Host = (): React.ReactElement => {
-  const [queue, setQueue] = React.useState([])
+  const [queue, setQueue] = React.useState<QueueEntry[]>([])
   const router = useRouter();
   const joinCode = "asdf"//router.query.joinCode;
 
   React.useEffect(()=>{
-    getInitialQueue(joinCode).then((res)=> setQueue(res));
+    getInitialQueue(joinCode).then((res: any)=> {setQueue(res)});
   })
 
   //TO-DO
@@ -26,7 +27,7 @@ const Host = (): React.ReactElement => {
        <h1 className={styles.title}>TO JOIN: Go to <a href={"http://karaoq.vercel.app"}>karaoq.vercel.app</a> and type in code <span className={styles.span}>{joinCode}</span></h1>
        {queue.length > 0 ?
         <iframe className={styles.video}
-          src={"https://www.youtube.com/embed/" + queue[0].video_id}
+          src={"https://www.youtube.com/embed/" + queue[0].videoId}
         ></iframe> : <h3>{"No songs in queue. :("}</h3>}
     </main>
   );
